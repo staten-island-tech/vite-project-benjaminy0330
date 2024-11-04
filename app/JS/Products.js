bb5e5c27320e68c7f819a9bf0c13eeb06a6ab9e1
Benjamin Yee <benyee1209@gmail.com>
2024-11-04 13:57:18 -0500

    s
    s

---
+++ app/JS/Products.js
@@ -0,0 +1,23 @@
+const items =
+    [
+        { name: "Sushi", origin: "Japan", price: 15.99 },
+        { name: "Tacos", origin: "Mexico", price: 8.99 },
+        { name: "Pizza", origin: "Italy", price: 12.99 },
+        { name: "Croissant", origin: "France", price: 3.50 },
+        { name: "Peking Duck", origin: "China", price: 25.99 },
+        { name: "Paella", origin: "Spain", price: 18.99 },
+        { name: "Pierogi", origin: "Poland", price: 7.99 },
+        { name: "Baklava", origin: "Turkey", price: 5.00 },
+        { name: "Poutine", origin: "Canada", price: 6.99 },
+        { name: "Kimchi", origin: "Korea", price: 3.99 },
+        { name: "Biryani", origin: "India", price: 10.50 },
+        { name: "Bratwurst", origin: "Germany", price: 7.50 },
+        { name: "Feijoada", origin: "Brazil", price: 14.00 },
+        { name: "Falafel", origin: "Middle East", price: 5.50 },
+        { name: "Pho", origin: "Vietnam", price: 9.99 },
+        { name: "Empanada", origin: "Argentina", price: 3.25 },
+        { name: "Tom Yum Soup", origin: "Thailand", price: 7.75 },
+        { name: "Borscht", origin: "Russia", price: 6.50 },
+        { name: "Goulash", origin: "Hungary", price: 8.99 },
+        { name: "Fish and Chips", origin: "United Kingdom", price: 11.50 },
+    ]
