3f4b2b6aee6fdf233af2d7c68ac89b8ef5bf8f63
Benjamin <benyee1209@gmail.com>
2024-11-10 15:22:03 -0500

    we did it
    we did it

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,33 +1,117 @@
 import "../css/style.css"
 import {food} from "../JS/Products.js"
 
-const cheapfood = food.filter((food) => (food.price < 10))
-console.log(cheapfood)
 
-const expensivefood = food.filter((food) => (food.price > 10))
-console.log(expensivefood)
-
-const vegetariantrue = food.filter((food) => (food.vegetarian))
-console.log(vegetariantrue)
+document.querySelector(".theme").addEventListener("click", function() {
+    if(document.body.classList.contains("lively")){
+        document.body.classList.add("gloomy");
+        document.body.classList.remove("lively");
+    } else{
+        document.body.classList.add("lively");
+        document.body.classList.remove("gloomy");
+    }
+});
 
 const cardlocation = document.getElementById("cards");
-
+//default
 food.forEach((food) => {
-    const html = `
-    <div class = "card">
-        <h2 id = "name">${food.name}</h2>
-        <h4 id = "origin">${food.origin}</h4>
-        <h2 id = "price">${food.price}</h2>
-    </div>
-    `;
+        const html = `
+        <div class = "card">
+            <h2>${food.name}</h2>
+            <h2>${food.origin}</h2>
+            <h2>${food.price}</h2>
+        </div>
+        `;
+        cardlocation.insertAdjacentHTML("beforeend", html)
+})
 
-cardlocation.insertAdjacentHTML("beforeend", html)
 
+//all
+document.querySelector(".all").addEventListener("click", function(){
+    document.querySelector(".cards").innerHTML = "";
+    
+    food.forEach((food) => {
+        const html = `
+        <div class = "card">
+            <h2>${food.name}</h2>
+            <h2>${food.origin}</h2>
+            <h2>${food.price}</h2>
+        </div>
+        `;
+        cardlocation.insertAdjacentHTML("beforeend", html)
+        
+})
+})
+//cheap
+document.querySelector(".low").addEventListener("click", function(){
+    document.querySelector(".cards").innerHTML = "";
+    const cheapfood = food.filter((food) => (food.price < 8))
+    console.log(cheapfood)
+
+    cheapfood.forEach((food) => {
+        const html = `
+        <div class = "card">
+            <h2>${food.name}</h2>
+            <h2>${food.origin}</h2>
+            <h2>${food.price}</h2>
+        </div>
+        `;
+        cardlocation.insertAdjacentHTML("beforeend", html)
+
+        
+})
+})
+//expensive
+document.querySelector(".high").addEventListener("click", function(){
+    document.querySelector(".cards").innerHTML = "";
+    const expensivefood = food.filter((food) => (food.price > 8))
+    console.log(expensivefood)
+
+    expensivefood.forEach((food) => {
+        const html = `
+        <div class = "card">
+            <h2>${food.name}</h2>
+            <h2>${food.origin}</h2>
+            <h2>${food.price}</h2>
+        </div>
+        `;
+        cardlocation.insertAdjacentHTML("beforeend", html)
+
+        
+})
+})
+//vegan
+
+
+document.querySelector(".vegan").addEventListener("click", function(){
+    document.querySelector(".cards").innerHTML = "";
+    const vegetarian = food.filter(food => food.vegetarian);
+    console.log(vegetarian);
+
+    vegetarian.forEach((food) => {
+        const html = `
+        <div class = "card">
+            <h2>${food.name}</h2>
+            <h2>${food.origin}</h2>
+            <h2>${food.price}</h2>
+        </div>
+        `;
+        cardlocation.insertAdjacentHTML("beforeend", html)
+
+        
+})
 })
 
 
 
 
+/*const expensivefood = food.filter((food) => (food.price > 10))
+console.log(expensivefood)
 
+const vegetariantrue = food.filter((food) => (food.vegetarian))
+console.log(vegetariantrue)*/
 
+/*const vegetarianfood = [true];
+const vegetarian = food.filter(food => vegetarianfood.includes(food.vegetarian));
+console.log(vegetarian);*/
 
